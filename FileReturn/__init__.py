cc92ef7c4e663c700be21ec50c435f21ae96bd32
lewisgmorris <lewis.morris@gmail.com>
2020-04-30 07:21:19 +0100

    init

---
+++ FileReturn/__init__.py
@@ -0,0 +1,1 @@
+from FileReturn.func import file_return
---
+++ FileReturn/func.py
@@ -0,0 +1,45 @@
+from random import shuffle
+import os
+
+class file_return:
+
+    def __init__(self,directory="./", include_sub=True,return_list=False, prefix_ext=[".jpg"], ):
+        """
+        Simple class to return files from directory in list.
+        Can return single file name for random selection or full list.
+        Can search prefixes
+
+        :param directory: (str) Search directory
+        :param include_sub: (bool) - should include sub directories
+        :param return_list: (bool) - return list True / return random file False
+        :param prefix_ext: (list(str)) - list of file ext's to return use [] for all files.
+        """
+        self.directory = directory
+        self.include_sub = include_sub
+        self.return_list = return_list
+        self.prefix_ext = prefix_ext
+
+    def return_files(self):
+
+        import shutil
+
+        files_list = []
+        for root, dirs, files in os.walk(self.directory):
+
+            if len(files) != 0:
+                for fl in files:
+                    if "." + fl.split(".")[-1] in self.prefix_ext or self.prefix_ext == []:
+                        if root[-1] == "/":
+                            files_list.append(root + fl)
+                        else:
+                            files_list.append(root + "/" + fl)
+                        if root == self.directory and self.include_sub == False:
+                            break
+
+        if len(files_list) == 0:
+            return "No files found"
+        elif self.return_list:
+            return files_list
+        else:
+            shuffle(files_list)
+            return(files_list[0])
