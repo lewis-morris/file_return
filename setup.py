73155ff93c7e277d502423458621b4ae11ce5884
lewisgmorris <lewis.morris@gmail.com>
2020-04-30 07:26:47 +0100

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='file_return',
+    version='0.01',
+    packages=['FileReturn'],
+    url='https://github.com/lewis-morris/file_return',
+    license='MIT',
+    author='lewis',
+    author_email='lewis.morris@gmail.com',
+    description='basic random file returner'
+)
